2bb5849405a1394cb3ec836fe0f7b784f4bcfcf6
marcioug <marc.ioug@mekanoid.io>
2023-08-23 23:50:13 -0400

    back end connect

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000', // Replace with your Apollo Server's URL
+  uri: 'gretta-backend-foobian2w-marcsouth.vercel.app', // Replace with your Apollo Server's URL
   cache: new InMemoryCache(),
 });
 
